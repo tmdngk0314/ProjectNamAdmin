e58225cec8de11f0184cc8e91299291f949c0435
Lee dong hee <tmdngk0314@naver.com>
2022-05-05 15:50:48 +0900

    [ADD] 로그인 화면 확인 버튼 터치 리스너 추가

--- app/src/main/java/com/example/projectnamadmin/MainActivity.java
+++ app/src/main/java/com/example/projectnamadmin/MainActivity.java
@@ -2,16 +2,47 @@ package com.example.projectnamadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText test;
+        btnOK = (ImageButton) findViewById(R.id.btnOk);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SelectActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnOK.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        btnOK.setBackgroundResource(R.drawable.ok_touch);
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        btnOK.setBackgroundResource(R.drawable.ok);
+                        return false;
+                    default: return false;
+                }
+
+            }
+        });
+
     }
 }
