9d5c0b30bd117c5ac07aa54117a3050f84b97a8e
Lee dong hee <tmdngk0314@naver.com>
2022-05-10 15:21:09 +0900

    [ADD] 기능 선택버튼 touchlistener추가

--- app/src/main/java/com/example/projectnamadmin/SelectActivity.java
+++ app/src/main/java/com/example/projectnamadmin/SelectActivity.java
@@ -3,12 +3,63 @@ package com.example.projectnamadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class SelectActivity extends AppCompatActivity {
+    RelativeLayout firstRela, secondRela;
+    ImageButton lockermanage, noticemanage;
+
+    public boolean onTouchReserve(MotionEvent event, RelativeLayout a){
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                a.setBackgroundResource(R.drawable.select_box_touch);
+                return false;
+            case MotionEvent.ACTION_UP:
+                a.setBackgroundResource(R.drawable.select_box);
+                return false;
+            default: return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        firstRela = (RelativeLayout) findViewById(R.id.firstRela);
+        secondRela = (RelativeLayout) findViewById(R.id.secondRela);
+        lockermanage = (ImageButton) findViewById(R.id.img_lockermanage);
+        noticemanage = (ImageButton) findViewById(R.id.img_noticemanage);
+
+        firstRela.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                return onTouchReserve(event,firstRela);
+
+            }
+        });
+
+        secondRela.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                return onTouchReserve(event,secondRela);
+
+            }
+        });
+        lockermanage.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                return onTouchReserve(event,firstRela);
+
+            }
+        });
+        noticemanage.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                return onTouchReserve(event,secondRela);
+
+            }
+        });
+
+
     }
 }
