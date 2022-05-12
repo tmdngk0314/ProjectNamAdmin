870349e29e844540c2f89d8782df7f8c91839c9f
Lee dong hee <tmdngk0314@naver.com>
2022-05-12 16:46:30 +0900

    [ADD] 회원가입 디자인 추가

---
+++ app/src/main/java/com/example/projectnamadmin/LockerListActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectnamadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LockerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locker_list);
+    }
+}
