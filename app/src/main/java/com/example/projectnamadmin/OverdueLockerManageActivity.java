aad549a4e1c43abda2c8b6107ee48297d2c86231
Lee dong hee <tmdngk0314@naver.com>
2022-06-02 17:18:41 +0900

    [ADD] 연체 사물함 관리 디자인 작업

---
+++ app/src/main/java/com/example/projectnamadmin/OverdueLockerManageActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectnamadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OverdueLockerManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overdue_locker_manage);
+    }
+}
